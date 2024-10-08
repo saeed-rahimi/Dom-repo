5222c79e975dd49f8503d0311e46d74e3cf3866c
saeed rahimi <saeedteachcodee@email.com>
2024-10-08 10:07:04 +0330

    finished

--- tasl 1/script.js
+++ tasl 1/script.js
@@ -1,8 +1,9 @@
 "use strict";
 const text = document.querySelectorAll("p");
-text[0].textContent = "jello";
 text.forEach((mov, i) => {
   mov.textContent = `paragraph ${i + 1}`;
- mov.style.backgroundColor = "blue";
+ mov.style.backgroundColor = "lightblue";
+ mov.style.borderRadius = "16px";
 });
+text[0].textContent = "First paragraph has been updated!";
 
